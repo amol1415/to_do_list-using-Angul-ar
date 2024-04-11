633924e2f490b7de13030c6805af655b36d373b4
amol1415 <amolmaharnavar90@gmail.com>
2024-04-11 22:54:26 +0530

    Initial Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp1';
+
+
+  public items: any = [];
+
+  addData(data: any) {
+
+
+    this.items.push(data);
+    console.log(this.items);
+  }
+
+  deleteData(data: any) {
+    let index = this.items.indexOf(data);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+      console.log(`Deleted ${data}`);
+    }
+  }
 }
